9a3be6b504528e3cbb41020f53a9a5b72bd8fca3
Alar of Runetotem <alar@aspide.it>
2018-08-16 12:27:20 +0200

    relnotes

--- RelNotes.lua
+++ RelNotes.lua
@@ -7,12 +7,16 @@ self:HF_Paragraph("Description")
 self:Wiki([[
 = ChampionCommander helps you when choosing the right follower for the right mission =
 == General enhancements ==
+* It's basically a OrderHallCommander clone
 * Mission panel is movable (position not saved, it's jus to see things, panel is so huge...)
 * Success chance extimation shown in mission list (optionally considering only available followers)
 * Proposed party buttons and mission autofill
 * "What if" switches to change party composition based on criteria
 == Silent mode ==
 typing /BFA silent in chat will eliminate every chat message from ChampionCommander
+]])
+self:RelNotes(0,1,2, [[
+Fix: Fixing various things as I discover them
 ]])
 self:RelNotes(0,1,1, [[
 Feature: This is a clone of OrderHallCommander for BFA champions table
