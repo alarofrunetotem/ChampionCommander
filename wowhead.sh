e26f9c797cf0f482692938c174725c56df9a0b4f
Alar of Runetotem <alar@aspide.it>
2018-09-09 17:24:23 +0200

    Changed for champion equipments

--- wowhead.sh
+++ wowhead.sh
@@ -2,7 +2,4 @@
 cd $(dirname $0)
 echo -e "local me,ns = ...\n" >wowhead.lua
 date +"ns.wowhead_update=%s" >>wowhead.lua
-../wowhelpers/OCArtifacts.php >>wowhead.lua
-../wowhelpers/OCEquipment.php >>wowhead.lua
-../wowhelpers/OCRep.php >>wowhead.lua
-#../wowhelpers/GCGearTokens.php >>wowhead.lua
+../wowhelpers/CCEquipment.php >>wowhead.lua
