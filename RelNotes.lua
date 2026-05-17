cf0a5f22b2d29cf7c0c6436488125030b6813f6c
Alar of Runetotem <alar@aspide.it>
2026-05-17 21:52:19 +0200

    Toc: 12.0.5

--- RelNotes.lua
+++ RelNotes.lua
@@ -17,6 +17,9 @@ self:Wiki([[
 typing /BFA silent in chat will eliminate every chat message from ChampionCommander
 ]])
 self:HF_Paragraph('Release Notes')
+self:RelNotes(99,99,99,[[
+Toc: 12.0.5
+]])
 self:RelNotes(1,16,2,[[
 Fix: tutorial was not hiding on closing
 ]])
